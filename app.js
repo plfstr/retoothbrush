000409e22193e597a2a8e0b6febfec1baba388e6
Paul Foster <paulfosterdesign@outlook.com>
2021-01-30 18:03:35 +0000

    Revert "Revert "Merge branch 'master' into rtb-brushdatenowrefactor""
    This reverts commit 91eb5a6f53d05f44552741746df99e88d97158b6.

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ let localesCheck = (() => {
 */
 function langChange() {
 	if ( confirm('Browser language has changed. Reload with new language preferences?') ) {
-		console.warn('brushDates updated, reloaded?');
+		console.warn('datechanged updated, reloaded?');
 	}
 }
 
@@ -126,20 +126,31 @@ function dateUtc( dateIn ){
 }
 
 
-function makeDates( dateBrushchange = false) {
+/*
+* @class makeDates
+*/
+class makeDates {
 
-	if ( dateValid(dateBrushchange) ) {
-		
-		let	dateStart = moment(dateBrushchange, "YYYY-MM-DD");
-		
-		let	dateEnd = moment(dateStart).add(90, 'days');
-		
-		let	dateDayremain = Math.max(0, dateEnd.diff(moment(), 'days') );
-		
-		let brushDates = Array.from([dateStart, dateDayremain, dateEnd]);
-		
-		dateFill(brushDates);
+	constructor (datechanged) {
+		this.date = moment(datechanged, "YYYY-MM-DD");
 	}
+
+	_dateStart() {
+		return this.date;
+	}
+
+	_dateEnd() {
+		return moment(this.date).add(90, 'days');
+	}
+	
+	_dateDayremain() {
+		return Math.max(0, this._dateEnd().diff(moment(), 'days') );
+	}
+	
+	get brushDates() {
+		return {datestart: this._dateStart(), dateremain: this._dateDayremain(), dateend: this._dateEnd()}
+	}
+	
 }
 
 
@@ -165,26 +176,32 @@ function dateFormat() {
 /*
 * Add dates to DOM
 * @function dateFill
-* @param {array} brushDates - Passes array of start, days remaining, end date
+* @param {array} datechanged - Passes array of start, days remaining, end date
 */
-function dateFill(brushDates) {
-	
+function dateFill(datechanged) {
+
+	if ( dateValid(datechanged) ) {
+		
+		let {datestart, dateremain, dateend} = new makeDates(datechanged).brushDates;
+		
 		// Vars
 		let domDaystart = document.querySelector('#dayStart');
 		let	domDayremain = document.querySelector('#dayRemaining');
 		let	domDayend = document.querySelector('#dayEnd');		
 		
 		// Date Start		
-		domDaystart.textContent = brushDates[0].format( dateFormat() );
-		domDaystart.setAttribute('datetime', `${dateUtc(brushDates[0])}`);
+		domDaystart.textContent = datestart.format('DD/MM/YYYY');
+		domDaystart.setAttribute('datetime', `${dateUtc(datestart)}`);
 
 		// Days Remain
-		domDayremain.textContent = `${ dayPlural(brushDates[1]) }`;
-		domDayremain.setAttribute('datetime', `P ${brushDates[1]} D`);
+		domDayremain.textContent = `${ dayPlural(dateremain) }`;
+		domDayremain.setAttribute('datetime', `P ${dateremain} D`);
 		
 		// Date End
-		domDayend.textContent = brushDates[2].format( dateFormat() );
-		domDayend.setAttribute('datetime', `${dateUtc(brushDates[2])}`);
+		domDayend.textContent = dateend.format('DD/MM/YYYY');
+		domDayend.setAttribute('datetime', `${dateUtc(dateend)}`);
+	
+	}
 		
 }
 
@@ -197,7 +214,7 @@ function dateFill(brushDates) {
 function brushDate() {
 	
 	if ( storedDate ) {
-		makeDates(storedDate);
+		dateFill(storedDate);
 	} else {
 		return;
 	}
@@ -219,13 +236,14 @@ function brushSwap() {
 		if (storedDate) {
 			confirm('Brush Changed. Create new date?');
 		}
-		makeDates(datenow);
+		dateFill(datenow);
 		store.set('dateSwapped', datenow);
 	}
 	
 	if (hasScheduling) {
 		try {
 			createScheduledNotification('retoothbrush', 'ReToothbrush', moment().valueOf());
+			document.querySelector('i').textContent = moment().add(1, 'days').valueOf();
 		} catch(error) {
 			console.warn(error);
 			confirm('Notification failed to schedule. You will not receive a reminder');
@@ -297,13 +315,13 @@ if (hasScheduling) {
 
 	createScheduledNotification = async (tag, title, timestamp) => {
 		console.log({tag, title, timestamp});
-		let scheduleDelay = moment().add(1, 'days').valueOf(); // 1 day
+		let timedelay = 1000 * 60 * 60 * 24;
 		const registration = await navigator.serviceWorker.getRegistration();
 		console.log(registration);
 		registration.showNotification(title, {
 			tag: tag,
 			body: "Its time to swap your toothbrush!",
-			showTrigger: new TimestampTrigger(timestamp + scheduleDelay)
+			showTrigger: new TimestampTrigger(timestamp + timedelay)
 		});
 	};
 
